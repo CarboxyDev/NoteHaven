ffb36c4c9ef92e690e948ca82f6b46f8ac977fd1
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-04-13 00:23:58 +0530

    Add minor styling changes to PoweredSection

--- src/components/PoweredSection.tsx
+++ src/components/PoweredSection.tsx
@@ -2,25 +2,13 @@ const PoweredSection = () => {
   return (
     <>
       <div className="bg-blue banner flex h-80 items-center justify-center">
-        <h2 className="rounded-2xl bg-white px-12 py-10 font-inter text-2xl font-semibold text-gray-700 sm:text-4xl">
+        <h2 className="rounded-2xl bg-white px-12 py-10 font-inter text-2xl font-semibold text-gray-700 shadow-md transition delay-200 duration-300 hover:bg-slate-100 sm:text-4xl">
           Powered by <span className="magic-text-static">IPFS</span> and{" "}
           <span className="magic-text-static">Filecoin</span>
         </h2>
       </div>
     </>
   );
-
-  // return (
-  //   <>
-  //     <div className="bg-blue banner flex min-h-[300px] items-center justify-center">
-  //       <img
-  //         src="/powered-graphic.png"
-  //         alt="powered-by"
-  //         className="h-[183px] w-[472px]"
-  //       />
-  //     </div>
-  //   </>
-  // );
 };
 
 export default PoweredSection;
